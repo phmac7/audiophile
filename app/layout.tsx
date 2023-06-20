ee3b73f248e78466500fd640a2a348c83a95ab87
Pedro Machado <phvieiras@hotmail.com>
2023-06-19 23:07:59 -0300

    feat: graphQL codegen config

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import GetHeader from '@/helpers/GetHeader';
 import '@/styles/globals.css';
 import { Manrope } from 'next/font/google';
 import styles from '@/styles/layout.module.scss';
+import { ApolloWrapper } from '@/lib/apollo-wrapper';
 
 const manrope = Manrope({ subsets: ['latin'] });
 
@@ -19,10 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={manrope.className}>
-        <Navbar />
-        <GetHeader />
-        <main className={styles.layout}>{children}</main>
-        <Footer />
+        <ApolloWrapper>
+          <Navbar />
+          <GetHeader />
+          <main className={styles.layout}>{children}</main>
+          <Footer />
+        </ApolloWrapper>
       </body>
     </html>
   );
---
+++ lib/apollo-wrapper.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+const endpoint = process.env.ENDPOINT_CONTENTFUL as string;
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  SuspenseCache,
+} from '@apollo/client';
+import {
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from '@apollo/experimental-nextjs-app-support/ssr';
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    // https://studio.apollographql.com/public/spacex-l4uc6p/
+    uri: endpoint,
+    headers: {
+      authorization: `Bearer ${process.env.CONTENT_DELIVERY_KEY}`,
+    },
+  });
+
+  return new ApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === 'undefined'
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+function makeSuspenseCache() {
+  return new SuspenseCache();
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider
+      makeClient={makeClient}
+      makeSuspenseCache={makeSuspenseCache}
+    >
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
