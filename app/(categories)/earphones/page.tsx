7fb4436f6c37c13f547eaf843392f3cf70000429
Pedro Machado <phvieiras@hotmail.com>
2023-11-15 23:41:58 -0300

    fix: export default

--- app/(categories)/earphones/page.tsx
+++ app/(categories)/earphones/page.tsx
@@ -3,7 +3,7 @@ import { getClient } from '@/graphQL/graphQLClient';
 import { GET_PRODUCTLIST } from '@/graphQL/queries';
 import { GetProductListQuery } from '@/graphQL/schema';
 
-export default async function Earphones() {
+const Earphones = async () => {
   const client = getClient();
   const { data }: { data: GetProductListQuery } = await client.query({
     query: GET_PRODUCTLIST,
@@ -11,4 +11,5 @@ export default async function Earphones() {
   });
   const productList = data.productCollection?.items;
   return <ProductList productList={productList} />;
-}
+};
+export default Earphones;
